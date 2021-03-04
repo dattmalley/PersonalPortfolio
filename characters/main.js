d526f25a61e40982fd28b8e84bd00361ee2cae0d
Matt Dalley <dattmalley@gmail.com>
2021-03-04 12:58:28 -0700

    fixed css on characters and films

--- characters/main.js
+++ characters/main.js
@@ -2,24 +2,49 @@ import { people } from '../data/people.js'
 
 const mainContent = document.querySelector('main')
 
-people.forEach(person => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    charImg.src = "https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg"
-    const charCaption = document.createElement('figcaption')
+const mainHeader = document.createElement('header')
 
-    charCaption.textContent = perosn.name
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
 
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-    mainContent.appendChild(charFigure)
+mainHeader.appendChild(maleButton)
+document.body.insertBefore(mainHeader, mainContent)
+maleButton.addEventListener('click', () => {
+    populateDOM(maleCharacters)
 })
 
-funct getLastNumber(url) {
+const maleCharacters = people.filter(person => person.gender === 'male') 
+const femaleCharacters = people.filter(person => person.gender === 'female') 
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' || person.gender === 'none') {
+        return person
+    }
+}) 
+
+console.log(maleCharacters)
+
+function populateDOM(characters) {
+    characters.forEach(person => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(person.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        const charCaption = document.createElement('figcaption')
+    
+        charCaption.textContent = person.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+        mainContent.appendChild(charFigure)
+    })
+}
+
+
+function getLastNumber(url) {
     let end = url.lastIndexOf('/')
     let start = end - 2
     if (url.charAt(start) === '/') {
         start++
     }
-    return url.slice{start, end}
+    return url.slice(start, end)
 }
