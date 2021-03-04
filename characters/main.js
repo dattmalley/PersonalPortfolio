4fb9a14237575699ee3df9550e46030228b254d6
Matt Dalley <dattmalley@gmail.com>
2021-03-04 13:00:23 -0700

    other characters showing

--- characters/main.js
+++ characters/main.js
@@ -10,7 +10,7 @@ maleButton.textContent = 'Male Characters'
 mainHeader.appendChild(maleButton)
 document.body.insertBefore(mainHeader, mainContent)
 maleButton.addEventListener('click', () => {
-    populateDOM(maleCharacters)
+    populateDOM(otherCharacters)
 })
 
 const maleCharacters = people.filter(person => person.gender === 'male') 
