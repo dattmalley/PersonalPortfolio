e487e5954789bbba06ae11dc457dcb7e60383685
Matt Dalley <dattmalley@gmail.com>
2021-04-20 16:37:09 -0600

    removed vh bc it blocks content added modal

--- starships/main.js
+++ starships/main.js
@@ -4,6 +4,19 @@ import { removeChildren, getLastNumber } from '../utils/index.js'
 const main = document.querySelector('main')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.shipView')
+const dialog = document.querySelector('.modal')
+
+const closeButton = document.querySelector('.modal-close')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+}) 
+
+const modalBackground = document.querySelector('.modal-background')
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
 
 function populateNav() {
     starships.forEach((starship) => {
@@ -26,6 +39,7 @@ function populateShipView(shipData) {
     shipImage.addEventListener('error', () => {
         console.log('Oops! Ship in Garage!')
         shipImage.hidden= true
+        dialog.classList.toggle("is-active")
     })
     shipView.appendChild(shipImage)
 }
