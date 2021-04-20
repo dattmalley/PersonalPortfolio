66f6d54dea87f8621e867740103b927faf101b6b
Matt Dalley <dattmalley@gmail.com>
2021-04-20 14:14:01 -0600

    styled background like stars

--- starships/main.js
+++ starships/main.js
@@ -34,6 +34,25 @@ populateNav()
 
 function addStarField(element, numStars) {
     element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('backround-color', 'white')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy}[1]px`
+        star.style.setProperty('position', 'absolute')
+        element.appendChild(star)
+    }
+}
+
+fnction getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return (randomX, randomY)
 }
 
 addStarField(document.querySelector('body', 1000))
