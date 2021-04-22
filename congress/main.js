736c178a486c23e3e2c51de2a5cf5c9e1ab4d7f9
Matt Dalley <dattmalley@gmail.com>
2021-04-22 13:13:34 -0600

    congress images and styling

--- congress/main.js
+++ congress/main.js
@@ -1,21 +1,39 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
 
-const repubButton = document.querySelector('#republicans')
+const congressGrid = document.querySelector('.congressGrid')
+const senioritySort = document.querySelector('#senioritySort')
+const birthdaySort = document.querySelector('#birthdaySort')
 
-repubButton.addEventListener('click', () => {
-    showRepublicans()
-})
+function populateCongressGrid(simplifiedList) {
+    console.log(simplifiedList)
+    simplifiedList.forEach(person => {
+        let personDiv = document.createElement('div')
+        let personFig = document.createElement('figure')
+        let figImage = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
 
-function showRepublicans() {
-    //const repubs = representatives.filter(rep => rep.party === 'R')
-    const repubs = representatives.map(rep => {
-        let smallRepub = {}
-        if (rep.party === 'R') {
-                smallRepub.id = rep.id,
-                smallRepub.name =`${rep.first_name} ${rep.middle_name} ${rep.last_name}`
-            }
-            return smallRepub
-    })
-    console.log(repubs)
+        figImage.src= person.imgURL
+        figCaption.textContent = person.name
+
+        personFig.appendChild(figImage)
+        personFig.appendChild(figCaption)
+        personDiv.appendChild(personFig)
+        congressGrid.appendChild(personDiv)
+    });
 }
+
+function getSimplifiedPeople(peopleList) {
+    return peopleList.map(person => {
+        let middleName = person.middle_name ? ` ${person.middle_name}` : ``
+        return {
+            id: person.id,
+            name: `${person.first_name} ${middleName} ${person.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-200px.jpeg`
+        }
+    })
+}
+
+populateCongressGrid(getSimplifiedPeople(representatives))
+
+
