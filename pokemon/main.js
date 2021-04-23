d77d0e19a63ca978758e6c15cb4e67645703379b
Matt Dalley <dattmalley@gmail.com>
2021-04-22 23:50:39 -0600

    added pokecode

--- pokemon/main.js
+++ pokemon/main.js
@@ -26,5 +26,17 @@ function loadPage() {
 }
 
 function populatePokeCard(singlePokemon) {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+
     console.log(singlePokemon)
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card card__face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
 }
