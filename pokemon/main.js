67a46bac69f328c348c023159df189523dd3af4c
Matt Dalley <dattmalley@gmail.com>
2021-04-29 21:31:48 -0600

    pokeImages showing via server

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,17 +1,27 @@
-const pokeGrid = document.querySelector(".pokeGrid")
-const loadButton = document.querySelector(".loadPokemon")
+const pokeGrid = document.querySelector(".pokeGrid");
+const loadButton = document.querySelector(".loadPokemon");
+const fetchButton = document.querySelector(".fetchPokemonByID");
 
 loadButton.addEventListener("click", () => {
-  loadPage()
-})
+  loadPage();
+});
+
+fetchButton.addEventListener("click", () => {
+  let pokeId = prompt("Pokemon ID or Name:");
+  console.log(pokeId)
+  getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+    .then((data) => populatePokeCard(data))
+    // .catch(error => console.log(error));
+});
 
 async function getAPIData(url) {
   try {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    alert("Image doesn't exist.")
   }
 }
 
@@ -19,56 +29,57 @@ function loadPage() {
   getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
     async (data) => {
       for (const singlePokemon of data.results) {
-        await getAPIData(singlePokemon.url).then(
-            (pokeData) => populatePokeCard(pokeData)
-        )
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        );
       }
     }
-  )
+  );
 }
 
 function populatePokeCard(singlePokemon) {
-  let pokeScene = document.createElement("div")
-  pokeScene.className = "scene"
-  let pokeCard = document.createElement("div")
-  pokeCard.className = "card"
+  let pokeScene = document.createElement("div");
+  pokeScene.className = "scene";
+  let pokeCard = document.createElement("div");
+  pokeCard.className = "card";
   pokeCard.addEventListener("click", () => {
-    pokeCard.classList.toggle("is-flipped")
-  })
+    pokeCard.classList.toggle("is-flipped");
+  });
   // card front
-  pokeCard.appendChild(populateCardFront(singlePokemon))
+  pokeCard.appendChild(populateCardFront(singlePokemon));
   // card back
-  pokeCard.appendChild(populateCardBack(singlePokemon))
+  pokeCard.appendChild(populateCardBack(singlePokemon));
   //append the all to pokeGrid
-  pokeScene.appendChild(pokeCard)
-  pokeGrid.appendChild(pokeScene)
+  pokeScene.appendChild(pokeCard);
+  pokeGrid.appendChild(pokeScene);
 }
 
 function populateCardFront(pokemon) {
-  let pokeFront = document.createElement("div")
-  pokeFront.className = "card__face card__face--front"
-  let frontLabel = document.createElement("h3")
-  frontLabel.textContent = pokemon.name
-  let frontImage = document.createElement("img")
-  frontImage.src = `images/${getImageFileName(pokemon)}.png`
-  pokeFront.appendChild(frontImage)
-  pokeFront.appendChild(frontLabel)
-  return pokeFront
+  let pokeFront = document.createElement("div");
+  pokeFront.className = "card__face card__face--front";
+  let frontLabel = document.createElement("h3");
+  frontLabel.textContent = pokemon.name;
+  let frontImage = document.createElement('img');
+  frontImage.src = getImageFileName(pokemon)
+  pokeFront.appendChild(frontImage);
+  pokeFront.appendChild(frontLabel);
+  return pokeFront;
 }
 
 function populateCardBack(pokemon) {
-  let pokeBack = document.createElement("div")
-  pokeBack.className = "card__face card__face--back"
-  let backLabel = document.createElement("p")
-  backLabel.textContent = "Type(s)"
-  pokeBack.appendChild(backLabel)
-  return pokeBack
+  let pokeBack = document.createElement("div");
+  pokeBack.className = "card__face card__face--back";
+  let backLabel = document.createElement("p");
+  backLabel.textContent = "Type(s)";
+  pokeBack.appendChild(backLabel);
+  return pokeBack;
 }
 
 function getImageFileName(pokemon) {
-  if (pokemon.id < 10) {
-    return `00${pokemon.id}`
-  } else if (pokemon.id > 9 && pokemon.id < 100) {
-    return `0${pokemon.id}`
-  }
+  let pokeId
+  if (pokemon.id < 10) pokeId = `00${pokemon.id}`
+  if (pokemon.id > 9 && pokemon.id < 100) pokeId = `0${pokemon.id}`
+  if (pokemon.id > 99 && pokemon.id < 810) pokeId = pokemon.id
+
+  return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeId}.png`
 }
