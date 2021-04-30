fac16977b13eaf0ae5eb862be2b2244b1953e5d6
Matt Dalley <dattmalley@gmail.com>
2021-04-30 00:46:42 -0600

    filter party and icons added

--- congress/main.js
+++ congress/main.js
@@ -5,6 +5,21 @@ import { removeChildren } from '../utils/index.js'
 const congressGrid = document.querySelector('.congressGrid')
 const seniorityButton = document.querySelector('#senioritySort')
 const birthdaySort = document.querySelector('#birthdaySort')
+const republicansButton = document.querySelector('#republicans')
+const democratsButton = document.querySelector('#democrats')
+const independentsButton = document.querySelector('#independents')
+
+republicansButton.addEventListener('click', () => {
+    populateCongressDiv(filterCongressPeople(senators, 'R'))
+})
+
+democratsButton.addEventListener('click', () => {
+    populateCongressDiv(filterCongressPeople(senators, 'D'))
+})
+
+independentsButton.addEventListener('click', () => {
+    populateCongressDiv(filterCongressPeople(senators, 'ID'))
+})
 
 seniorityButton.addEventListener('click', () => senioritySort())
 
@@ -12,13 +27,18 @@ function populateCongressDiv(simplifiedList) {
     removeChildren(congressGrid)
     simplifiedList.forEach(person => {
         let personDiv = document.createElement('div')
+        personDiv.className = 'figureDiv'
         let personFig = document.createElement('figure')
         let figImage = document.createElement('img')
         let figCaption = document.createElement('figcaption')
+        let partyIcon = document.createElement('i')
+        if (person.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (person.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (person.party === 'ID') partyIcon.className = 'fab fa-angellist'
 
         figImage.src= person.imgURL
         figCaption.textContent = person.name
-
+        figCaption.appendChild(partyIcon)
         personFig.appendChild(figImage)
         personFig.appendChild(figCaption)
         personDiv.appendChild(personFig)
@@ -33,7 +53,8 @@ function getSimplifiedPeople(peopleList) {
             id: person.id,
             name: `${person.first_name} ${middleName} ${person.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-200px.jpeg`,
-            seniority: parseInt(person.seniority, 10)
+            seniority: parseInt(person.seniority, 10),
+            party: person.party
         }
     })
 }
@@ -42,6 +63,8 @@ function senioritySort () {
     populateCongressDiv(getSimplifiedPeople(senators).sort((a, b) => a.seniority - b.seniority).reverse())
 }
 
+const filterCongressPeople = (chamber, politicalParty) => {
+    return getSimplifiedPeople(chamber).filter(member => member.party === politicalParty)
+}
+
 populateCongressDiv(getSimplifiedPeople(senators))
-
-
